04c604214cfde7401f3a4dd14aea45e141b11aad
Innocenzo Biondo <innocenzobiondo@gmail.com>
2016-10-10 15:06:16 +0200

    update post and get with promise

--- es2/views/app.js
+++ es2/views/app.js
@@ -1,11 +1,26 @@
-var routerApp = angular.module('es2', ['ui.router']);
+var routerApp = angular.module('es2', ['ui.router','ngResource']);
+
+routerApp.factory('ListContact',['$resource', function($resource) {
+  return $resource('/api/contacts/', {
+    query: {
+      method: 'GET', isArray:true
+    }
+  });
+}]);
+
+routerApp.factory('AddContact',['$resource', function($resource) {
+  return $resource('/api/contacts/', {
+    save: {
+      method: 'POST'
+    }
+  });
+}]);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/home');
 
   $stateProvider
-
   // HOME STATES AND NESTED VIEWS ========================================
   .state('home', {
     url: '/home',
@@ -15,33 +30,39 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
   .state('home.contacts', {
     url: '/contacts',
     templateUrl: 'partial-home-list.html',
-    controller: ['$scope','$http',
-    function ContactListController($scope,$http) {
+    controller: ['$scope','$http','ListContact','AddContact',
+    function ContactListController($scope,$http,ListContact,AddContact) {
       $scope.orderProp = "name";
       // we will store all of our form data in this object
       $scope.formData = {};
       $scope.SendData = function () {
-        var data ={
+
+        var data = {
           name1:    $scope.formData.name,
           surname: $scope.formData.surname,
           tel: $scope.formData.tel
         };
         console.log(data);
-        $http.post('/api/contacts', data )
-        .then(function(response) {
-          console.log(response);
-          $scope.contacts.push(response.data);
-        }).catch(function(response) {
-          console.error('Gists error', response.status, response.data);
-        });
+        return AddContact
+                        .save(data)
+                        .$promise
+                        .then(function(res){
+                          console.log(res);
+                          $scope.contacts.push(res);
+                        }).catch(function(response) {
+                          console.error('Gists error', response, response.data);
+                        });
       };
-      $http.get("/api/contacts").then(function(response) {
-        // $scope.contacts = data;
-        console.log(response);
-        $scope.contacts = response.data;
-      }).catch(function(response) {
-        console.error('Gists error', response, response.data);
-      });
+
+      ListContact
+                .query()
+                .$promise
+                .then(function(res){
+                  console.log(res);
+                  $scope.contacts = res;
+                }).catch(function(response) {
+                  console.error('Gists error', response, response.data);
+                });
     }
   ]
 });
